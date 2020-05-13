ed88bac519883ecb95fd32914aff5ff70a4a1579
SarmadNoor <43487713+SarmadNoor@users.noreply.github.com>
2020-05-13 01:09:25 -0400

    template driven form

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserLogin } from '../user-login';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  roles = ['Admin', 'Developer', 'QA'];
+  model = new UserLogin(); 
+  validated:boolean = false;
+  onSubmit() {
+    this.validated = true;
+  }
+
+}
